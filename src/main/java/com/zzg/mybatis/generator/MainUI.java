8698ca589231155b1211e897f2c1cb95b204a309
DESKTOP-7K928KD\zxsharp <hornet_team@sina.com>
2021-04-21 10:09:03 +0800

    submit some changes 1.repair bug 2.add favourite.png 3.reference png

--- src/main/java/com/zzg/mybatis/generator/MainUI.java
+++ src/main/java/com/zzg/mybatis/generator/MainUI.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +29,8 @@ public class MainUI extends Application {
 		Parent root = fxmlLoader.load();
 		primaryStage.setResizable(true);
 		primaryStage.setScene(new Scene(root));
+		primaryStage.getIcons().add(new Image(
+				MainUI.class.getResourceAsStream("/icons/favourite.png")));
 		primaryStage.show();
 
 		MainUIController controller = fxmlLoader.getController();
